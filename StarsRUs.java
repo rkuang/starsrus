a46a63e11070f5e75d6438ee1bf3d72bdec92348
Alan Tran <alanthetran@gmail.com>
2017-12-01 21:53:36 -0800

    fix indentation

--- StarsRUs.java
+++ StarsRUs.java
@@ -1,62 +1,62 @@
 import java.util.*;
 
 public class StarsRUs {
-    
-    public static void main(String[] args) {
 
-	ArrayList<String> validInputs = new ArrayList<>();
-	validInputs.add("login");
-	validInputs.add("register");
-	validInputs.add("exit");
-	
-	printBanner();
+  public static void main(String[] args) {
 
-	String input = "";
-	System.out.println("\n****\n");
+    ArrayList<String> validInputs = new ArrayList<>();
+    validInputs.add("login");
+    validInputs.add("register");
+    validInputs.add("exit");
 
-	while (!input.equals("exit")) {
-	    input = getUserInput();
-	    if (validInputs.contains(input)) {
-		switch (input) {
-		case "login":
-		    Customer.login();
-		    break;
-		case "register":
-		    Customer.register();
-		    break;
-		}
-	    } else {
-		System.out.println(String.format("'%s' is not a valid input", input));
-	    }
-	}
+    printBanner();
 
-	//RkuangDB db = new RkuangDB();
-	//db.getTestTable();
-	//db.closeConnection();
-	System.exit(0);
+    String input = "";
+    System.out.println("\n****\n");
+
+    while (!input.equals("exit")) {
+      input = getUserInput();
+      if (validInputs.contains(input)) {
+        switch (input) {
+          case "login":
+          Customer.login();
+          break;
+          case "register":
+          Customer.register();
+          break;
+        }
+      } else {
+        System.out.println(String.format("'%s' is not a valid input", input));
+      }
     }
 
-    private static void printBanner() {
-	System.out.println("");
-	System.out.println("");
-	System.out.println("   .d88888b    dP                                  888888ba     dP     dP            ");
-	System.out.println("   88.    \"'   88                                  88    `8b    88     88            ");
-	System.out.println("   `Y88888b. d8888P .d8888b. 88d888b. .d8888b.    a88aaaa8P'    88     88 .d8888b.   ");
-	System.out.println("         `8b   88   88'  `88 88'  `88 Y8ooooo.     88   `8b.    88     88 Y8ooooo.   ");
-	System.out.println("   d8'   .8P   88   88.  .88 88             88     88     88    Y8.   .8P       88   ");
-	System.out.println("    Y88888P    dP   `88888P8 dP       `88888P'     dP     dP    `Y88888P' `88888P'   ");
-	System.out.println("ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
-	System.out.println("");
-	System.out.println("Ricky Kuang, Alan Tran                                                     CMPSC 174A");
-	System.out.println("=====================================================================================");
-	System.out.println("Welcome to Stars R Us!");
-	System.out.println("Type 'help' for help");
-	System.out.println("");
-    }
+    //RkuangDB db = new RkuangDB();
+    //db.getTestTable();
+    //db.closeConnection();
+    System.exit(0);
+  }
 
-    private static String getUserInput() {
-	System.out.print(" > ");
-        Scanner in = new Scanner(System.in);
-        return(in.next());
-    }
+  private static void printBanner() {
+    System.out.println("");
+    System.out.println("");
+    System.out.println("   .d88888b    dP                                  888888ba     dP     dP            ");
+    System.out.println("   88.    \"'   88                                  88    `8b    88     88            ");
+    System.out.println("   `Y88888b. d8888P .d8888b. 88d888b. .d8888b.    a88aaaa8P'    88     88 .d8888b.   ");
+    System.out.println("         `8b   88   88'  `88 88'  `88 Y8ooooo.     88   `8b.    88     88 Y8ooooo.   ");
+    System.out.println("   d8'   .8P   88   88.  .88 88             88     88     88    Y8.   .8P       88   ");
+    System.out.println("    Y88888P    dP   `88888P8 dP       `88888P'     dP     dP    `Y88888P' `88888P'   ");
+    System.out.println("ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+    System.out.println("");
+    System.out.println("Ricky Kuang, Alan Tran                                                     CMPSC 174A");
+    System.out.println("=====================================================================================");
+    System.out.println("Welcome to Stars R Us!");
+    System.out.println("Type 'help' for help");
+    System.out.println("");
+  }
+
+  private static String getUserInput() {
+    System.out.print(" > ");
+    Scanner in = new Scanner(System.in);
+    return(in.next());
+  }
 }
