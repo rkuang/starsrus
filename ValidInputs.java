30513a48d227b915f3179dd39efe428f12b99e88
Ricky Kuang <rkuang26@gmail.com>
2017-12-02 19:25:49 -0800

    data for market accounts

--- ValidInputs.java
+++ ValidInputs.java
@@ -4,7 +4,8 @@ public class ValidInputs {
 
   ArrayList<String> activeState;
   ArrayList<String> noUser = new ArrayList<>(Arrays.asList("help","login","register","exit"));
-  ArrayList<String> loggedIn = new ArrayList<>(Arrays.asList("help","test","logout","exit"));
+  ArrayList<String> loggedIn = new ArrayList<>(Arrays.asList("help","customer","logout","exit"));
+  ArrayList<String> loggedIn = new ArrayList<>(Arrays.asList("help","admin","logout","exit"));
 
   public ValidInputs() {
     activeState = noUser;
@@ -16,7 +17,11 @@ public class ValidInputs {
       activeState = noUser;
       break;
       case "loggedIn":
-      activeState = loggedIn;
+      if (StarsRUs.activeUser.admin) {
+        activeState = admin;
+      } else {
+        activeState = loggedIn;
+      }
       break;
     }
   }
@@ -28,7 +33,7 @@ public class ValidInputs {
   public void print() {
     System.out.println("The following inputs are valid:");
     for (String input : activeState) {
-      System.out.println("   "+input);
+      System.out.println("     "+input);
     }
   }
 }
