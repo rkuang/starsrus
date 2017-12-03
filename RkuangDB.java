e349872a34a38dcbf0d6eb4dfaa0986278fb6330
Ricky Kuang <rkuang26@gmail.com>
2017-12-02 22:21:34 -0800

    bugs

--- RkuangDB.java
+++ RkuangDB.java
@@ -61,14 +61,14 @@ public class RkuangDB {
   }
 
   public Boolean updateBalance(double amount) {
-    String query = String.format("SELECT balance FROM Market_Accounts WHERE taxid='%s'", StarsRUs.activeUser.taxid)
+    String query = String.format("SELECT balance FROM Market_Accounts WHERE taxid='%s'", StarsRUs.activeUser.taxid);
 
     try (Statement statement = connection.createStatement()) {
       ResultSet rs = statement.executeQuery(query);
       if (rs.next()) {
-        int balance = rs.getDouble("balance");
-        if (balance - amount > 0) {
-          query = String.format("UPDATE Market_Accounts SET balance='%f' WHERE taxid='%d'", balance-amount, StarsRUs.activeUser.taxid);
+        double balance = rs.getDouble("balance");
+        if (balance+amount > 0) {
+          query = String.format("UPDATE Market_Accounts SET balance='%f' WHERE taxid='%d'", balance+amount, StarsRUs.activeUser.taxid);
           statement.executeUpdate(query);
           return true;
         } else {
