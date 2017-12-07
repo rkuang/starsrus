d1cac6bb5894e8904c6531a03d257ceed6ebac2b
Alan Tran <alan_tran@umail.ucsb.edu>
2017-12-07 15:16:08 -0800

    nested confusion

--- RkuangDB.java
+++ RkuangDB.java
@@ -208,7 +208,7 @@ public class RkuangDB {
         Double interest = calcAvgBalance(rs.getString("taxid")) * 0.03;
         try(Statement statement1 = connection.createStatement()){
           String addInterest = String.format("UPDATE Market_Accounts SET balance = '%f'", rs.getDouble("balance") + interest);
-          statement.executeUpdate(addInterest);
+          statement1.executeUpdate(addInterest);
         }catch(SQLException e){
           e.printStackTrace();
         }
