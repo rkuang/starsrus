5d23c1fc7833bd68b18fa4a830b005339b2db5bf
Alan Tran <alanthetran@gmail.com>
2017-12-07 00:23:43 -0800

    del

--- RkuangDB.java
+++ RkuangDB.java
@@ -170,7 +170,6 @@ public class RkuangDB {
   public void updateInterest(int future){
     int past = this.dayToInt(this.getDate());
     int days = future - past + 1;
-    system.println(days);
     String query = String.format("SELECT i.taxid,i.currentBal,i.daysHeld from Market_Accounts m, Interest i WHERE m.taxid = i.taxid AND m.balance = i.currentBal");
     try(Statement statement1 = connection.createStatement()){
       ResultSet rs = statement1.executeQuery(query);
