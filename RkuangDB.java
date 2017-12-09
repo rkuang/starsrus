b06fd59c40e9929394039c1f23396c28039251fa
Alan Tran <alanthetran@gmail.com>
2017-12-08 21:29:35 -0800

    ughh interest whyy

--- RkuangDB.java
+++ RkuangDB.java
@@ -372,6 +372,7 @@ public class RkuangDB {
     }catch(SQLException e){
       e.printStackTrace();
     }
+    /*
     try(Statement statement3 = connection.createStatement()){
       query = String.format("SELECT m.taxid,m.balance from Market_Accounts m, Interest i WHERE m.taxid = i.taxid AND m.balance <> i.currentBal");
       ResultSet rs = statement3.executeQuery(query);
@@ -385,7 +386,7 @@ public class RkuangDB {
       }
     } catch(SQLException e){
       e.printStackTrace();
-    }
+    }*/
   }
 
   public void calcInterest(){
